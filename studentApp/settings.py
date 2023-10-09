1dc4e36838f0b81bf35de99e658e576dcd39d3b1
Mamun <mamunurrashid.s.bd@gmail.com>
2023-10-09 15:47:12 +0600

    Custom Lifetime for Token and New Refresh create while Refresh

--- studentApp/settings.py
+++ studentApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,6 +29,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# Authentication Stuff
+SIMPLE_JWT = {
+'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+"ROTATE_REFRESH_TOKENS": False
+}
 # Application definition
 
 INSTALLED_APPS = [
